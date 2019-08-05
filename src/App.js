31781697c6f8f4845d8ea3ee4370e16e86317294
TopeBabz <newuser@News-MacBook-Air.local>
2019-08-04 23:50:52 -0400

    Beginning UI with React Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Form from "./Components/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  getRecipe = (e) => {
+      e.preventDefault();
+      console.log("Working");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Chef</h1>
+        </header>
+        <Form getRecipe={this.getRecipe}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Form = (props) => (
+	<form onSubmit={ props.getRecipe}>
+		<input type="text"/>
+		<button>Search</button>
+	</form>
+);
+
+export default Form;
