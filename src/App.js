3bacab92ce6dc791c1bf43af40dd800f97116fa1
TopeBabz <newuser@News-MacBook-Air.local>
2019-08-08 00:30:04 -0400

    Adding more UI: display rows and cols

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from "./Components/Form";
+import Recipes from "./Components/Recipes";
 
 const API_KEY = 'fd076c8f8ff6b79eb2ea3aaa369fab91';
 
@@ -12,15 +13,14 @@ class App extends Component {
 
   getRecipe = async (e) => {
 
-    // eslint-disable-next-line
-      const recipeName = e.target.elements.recipeName.value;
-      e.preventDefault();
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
 
-      const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=15`);
 
-      const data = await api_call.json();
-      this.setState({ recipes : data.recipes })
-      console.log(this.state.recipes);
+    const data = await api_call.json();
+    this.setState({ recipes : data.recipes })
+    console.log(this.state.recipes);
   }
 
   render() {
@@ -29,11 +29,8 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Recipe Chef</h1>
         </header>
-        <Form getRecipe={this.getRecipe}/>
-        <br />
-        { this.state.recipes.map((recipe) => {
-            return <p key={recipe.recipe_id}>{recipe.title}</p>
-        })}
+        <Form name={this.getRecipe}/>
+        <Recipes recipes={this.state.recipes}/>
       </div>
     );
   }
